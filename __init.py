109072248f4a07c1c3fbd70da6525560d278848a
CentralNervousSystem <boris.brumshtein@gmail.com>
2017-11-30 23:09:27 -0800

    Create __init.py

---
+++ __init.py
@@ -0,0 +1,12 @@
+import glob, os
+
+def main():
+    print("hello")
+    files = glob.glob1('dirname', 'pattern')
+    fout = fi
+    for f in files:
+        
+
+
+if __name__ == '__main__' : main()
+
